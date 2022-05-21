7099cbf87cd1b02b45b236d4c779bb7285c8b86d
Mark Olieman <molieman2002@gmail.com>
2022-05-21 15:32:43 +0200

    design profiel

--- src/main/java/nl/project/HelloApplication.java
+++ src/main/java/nl/project/HelloApplication.java
@@ -13,6 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setMaximized(true);
         stage.setTitle("Greetings!");
         stage.setScene(scene);
         stage.show();
